d4060fd7be454691d494499c1e41c7d731ea0e96
Colin Jackson <colin.s.jackson90@gmail.com>
2021-05-25 01:03:49 -0700

    complete 5b (more opcodes)

--- 05/lib/intcode_interpreter.rb
+++ 05/lib/intcode_interpreter.rb
@@ -22,6 +22,14 @@ class IntcodeInterpreter
         pointer = input_to_destination(pointer)
       when 4
         pointer = source_to_output(pointer, param_modes)
+      when 5
+        pointer = jump_if_true(pointer, param_modes)
+      when 6
+        pointer = jump_if_false(pointer, param_modes)
+      when 7
+        pointer = less_than(pointer, param_modes)
+      when 8
+        pointer = equals(pointer, param_modes)
       end
     end
 
@@ -63,13 +71,13 @@ class IntcodeInterpreter
     destination = program[pointer+2]
 
     program[destination] = input1 * input2
-    pointer += 3
+    pointer + 3
   end
 
   def input_to_destination(pointer)
     destination = program[pointer]
     program[destination] = input.gets.chomp.to_i
-    pointer += 1
+    pointer + 1
   end
 
   def source_to_output(pointer, param_modes)
@@ -77,6 +85,46 @@ class IntcodeInterpreter
     source = param_modes.first == 0 ? program[source_param] : source_param
 
     output.puts(source)
-    pointer += 1
+    pointer + 1
+  end
+
+  def jump_if_true(pointer, param_modes)
+    conditional, location = program[pointer..pointer+1].map.with_index do |param, i|
+      param_modes[i] == 0 ? program[param] : param
+    end
+
+    !conditional.zero? ? location : pointer + 2
+  end
+
+  def jump_if_false(pointer, param_modes)
+    conditional, location = program[pointer..pointer+1].map.with_index do |param, i|
+      param_modes[i] == 0 ? program[param] : param
+    end
+
+    conditional.zero? ? location : pointer + 2
+  end
+
+  def less_than(pointer, param_modes)
+    value1, value2 = program[pointer..pointer+1].map.with_index do |param, i|
+      param_modes[i] == 0 ? program[param] : param
+    end
+    destination = program[pointer+2]
+
+    is_less_than = value1 < value2 ? 1 : 0
+    program[destination] = is_less_than
+
+    pointer + 3
+  end
+
+  def equals(pointer, param_modes)
+    value1, value2 = program[pointer..pointer+1].map.with_index do |param, i|
+      param_modes[i] == 0 ? program[param] : param
+    end
+    destination = program[pointer+2]
+
+    is_less_than = value1 == value2 ? 1 : 0
+    program[destination] = is_less_than
+
+    pointer + 3
   end
 end
--- 05/spec/intcode_interpreter_spec.rb
+++ 05/spec/intcode_interpreter_spec.rb
@@ -13,50 +13,149 @@ RSpec.describe IntcodeInterpreter do
   end
 
   describe '#execute' do
-    context 'with a 99 opcode' do
-      it 'returns the program unchanged' do
-        interpreter = IntcodeInterpreter.new([99, 0, 1, 2])
-        expect(interpreter.execute).to    eq([99, 0, 1, 2])
+    context 'opcodes' do
+      context 'with opcode 99 (exit)' do
+        it 'returns the program unchanged' do
+          interpreter = IntcodeInterpreter.new([99, 0, 1, 2])
+          expect(interpreter.execute).to    eq([99, 0, 1, 2])
+        end
       end
-    end
 
-    context 'with a 1 opcode' do
-      it 'adds the referenced values and stores them at the referenced index' do
-        interpreter = IntcodeInterpreter.new([1, 0, 0, 1])
-        expect(interpreter.execute).to    eq([1, 2, 0, 1])
+      context 'with opcode 1 (add)' do
+        it 'adds the referenced values and stores them at the referenced index' do
+          interpreter = IntcodeInterpreter.new([1, 0, 0, 1])
+          expect(interpreter.execute).to    eq([1, 2, 0, 1])
+        end
       end
-    end
 
-    context 'with a 2 opcode' do
-      it 'multiplies the referenced values and stores them at the referenced index' do
-        interpreter = IntcodeInterpreter.new([2, 0, 0, 1])
-        expect(interpreter.execute).to    eq([2, 4, 0, 1])
+      context 'with opcode 2 (multiply)' do
+        it 'multiplies the referenced values and stores them at the referenced index' do
+          interpreter = IntcodeInterpreter.new([2, 0, 0, 1])
+          expect(interpreter.execute).to    eq([2, 4, 0, 1])
+        end
       end
-    end
 
-    context 'with a 3 opcode' do
-      it 'reads from a provided input and writes it to the referenced index' do
-        input = StringIO.new("777\n")
-        interpreter = IntcodeInterpreter.new([3, 0], input)
+      context 'with opcode 3 (input to destination)' do
+        it 'reads from a provided input and writes it to the referenced index' do
+          input = StringIO.new("777\n")
+          interpreter = IntcodeInterpreter.new([3, 0], input)
 
-        expect(interpreter.execute).to    eq([777, 0])
+          expect(interpreter.execute).to    eq([777, 0])
+        end
       end
-    end
 
-    context 'with a 4 opcode' do
-      it 'reads from a refenced index and writes it to output' do
-        output = StringIO.new
-        interpreter = IntcodeInterpreter.new([4, 0], nil, output)
-        interpreter.execute
+      context 'with opcode 4 (source to output)' do
+        it 'reads from a refenced index and writes it to output' do
+          output = StringIO.new
+          interpreter = IntcodeInterpreter.new([4, 0], nil, output)
+          interpreter.execute
 
-        expect(output.string).to eq("4\n")
+          expect(output.string).to eq("4\n")
+        end
+      end
+
+      context 'with opcode 5 (jump if true)' do
+        context 'when the first parameter is zero' do
+          it 'does nothing' do
+            interpreter = IntcodeInterpreter.new([5, 7, 5, 99, 1, 4, 4, 0])
+            expect(interpreter.execute).to    eq([5, 7, 5, 99, 1, 4, 4, 0])
+          end
+        end
+
+        context 'when the first parameter is nonzero' do
+          it 'moves the pointer to the value of the second parameter' do
+            interpreter = IntcodeInterpreter.new([5, 6, 5, 99, 1, 4, 4, 0])
+            expect(interpreter.execute).to    eq([2, 6, 5, 99, 1, 4, 4, 0])
+          end
+        end
+      end
+
+      context 'with opcode 6 (jump if false)' do
+        context 'when the first parameter is zero' do
+          it 'moves the pointer to the value of the second parameter' do
+            interpreter = IntcodeInterpreter.new([6, 7, 5, 99, 1, 4, 4, 0])
+            expect(interpreter.execute).to    eq([2, 7, 5, 99, 1, 4, 4, 0])
+          end
+        end
+
+        context 'when the first parameter is nonzero' do
+          it 'does nothing' do
+            interpreter = IntcodeInterpreter.new([6, 6, 5, 99, 1, 4, 4, 0])
+            expect(interpreter.execute).to    eq([6, 6, 5, 99, 1, 4, 4, 0])
+          end
+        end
+      end
+
+      context 'with opcode 7 (less than)' do
+        context 'when the first parameter is less than the second parameter' do
+          it 'writes 1 to the destination specified by the third parameter' do
+            interpreter = IntcodeInterpreter.new([7, 5, 4, 0, 99, 98])
+            expect(interpreter.execute).to    eq([1, 5, 4, 0, 99, 98])
+          end
+        end
+
+        context 'when the first parameter is not less than the second parameter' do
+          it 'writes 0 to the destination specified by the third parameter' do
+            interpreter = IntcodeInterpreter.new([7, 5, 4, 0, 99, 99])
+            expect(interpreter.execute).to    eq([0, 5, 4, 0, 99, 99])
+          end
+        end
+      end
+
+      context 'with opcode 8 (equal)' do
+        context 'when the first parameter is equal to the second parameter' do
+          it 'writes 1 to the destination specified by the third parameter' do
+            interpreter = IntcodeInterpreter.new([8, 5, 4, 0, 99, 99])
+            expect(interpreter.execute).to    eq([1, 5, 4, 0, 99, 99])
+          end
+        end
+
+        context 'when the first parameter is not less than the second parameter' do
+          it 'writes 0 to the destination specified by the third parameter' do
+            interpreter = IntcodeInterpreter.new([8, 5, 4, 0, 99, 100])
+            expect(interpreter.execute).to    eq([0, 5, 4, 0, 99, 100])
+          end
+        end
       end
     end
 
     context 'with nonzero parameter modes' do
-      it 'treats the corresponding parameter as a value and not an address' do
+      it 'treats the corresponding parameter as a value and not an address for opcode 1 (add)' do
         interpreter = IntcodeInterpreter.new([1101, 0, 0, 0])
         expect(interpreter.execute).to    eq([0,    0, 0, 0])
+      end
+
+      it 'treats the corresponding parameter as a value and not an address for opcode 2 (multiply)' do
+        interpreter = IntcodeInterpreter.new([1102, 0, 0, 0])
+        expect(interpreter.execute).to    eq([0,    0, 0, 0])
+      end
+
+      it 'treats the corresponding parameter as a value and not an address for opcode 4 (source to output)' do
+        output = StringIO.new
+        interpreter = IntcodeInterpreter.new([104, 3, 99, 777], nil, output)
+        interpreter.execute
+
+        expect(output.string).to eq("3\n")
+      end
+
+      it 'treats the corresponding parameter as a value and not an address for opcode 5 (jump if true)' do
+        interpreter = IntcodeInterpreter.new([105, 7, 5, 99, 1, 4, 4, 0])
+        expect(interpreter.execute).to    eq([2,   7, 5, 99, 1, 4, 4, 0])
+      end
+
+      it 'treats the corresponding parameter as a value and not an address for opcode 6 (jump if false)' do
+        interpreter = IntcodeInterpreter.new([106, 0, 5, 99, 1, 4, 4, 0])
+        expect(interpreter.execute).to    eq([2,   0, 5, 99, 1, 4, 4, 0])
+      end
+
+      it 'treats the corresponding parameter as a value and not an address for opcode 7 (less than)' do
+        interpreter = IntcodeInterpreter.new([1107, 5, 4, 0, 99, 98])
+        expect(interpreter.execute).to    eq([0,    5, 4, 0, 99, 98])
+      end
+
+      it 'treats the corresponding parameter as a value and not an address for opcode 8 (equals)' do
+        interpreter = IntcodeInterpreter.new([1108, 5, 4, 0, 99, 99])
+        expect(interpreter.execute).to    eq([0,    5, 4, 0, 99, 99])
       end
     end
 
